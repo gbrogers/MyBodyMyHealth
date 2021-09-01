1f08349b99669ff7e8147ccb6309c08b3161045f
Giselle Rogers <giselle.b.rogers@gmail.com>
2021-08-31 21:43:51 -0600

    displays api request but undefined

--- src/Resources.js
+++ src/Resources.js
@@ -1,40 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Resources() {
+  const [resourceList, setResourceList] = useState({});
+
   const baseURL =
     "https://health.gov/myhealthfinder/api/v3/myhealthfinder.json";
   // https://health.gov/myhealthfinder/api/v3/myhealthfinder.json?age=35&sex=female&pregnant=no&sexuallyActive=yes&tobaccoUse=no
 
-  const displayResource = (resources) => {
-    return (
-      <div className="personalizedResources">
-        {resources.map((item) => {
-          let url = item.AccessibleVersion;
-          let title = item.Title;
-          // console.log(url);
-          return <div key={item}>{title}</div>;
-        })}
-      </div>
-    );
-  };
+  useEffect(() => {
+    console.log(resourceList);
+    // console.log(Object.entries(resourceList));
+  }, [resourceList]);
 
   const generateResource = (age, sex, pregnant, sexActive, tobacco) => {
     console.log("in generateResource");
     axios
-      // .get(
-      //   "https://health.gov/myhealthfinder/api/v3/myhealthfinder.json?age=35&sex=female&pregnant=no&sexuallyActive=yes&tobaccoUse=no"
-      // )
       .get(
         `${baseURL}?age=${age}&sex=${sex}&pregnant=${pregnant}&sexuallyActive=${sexActive}&tobaccoUse=${tobacco}`
       )
       .then((res) => {
-        // console.log(res.data.result.result.resources);
-        let resources = res.data.Result.Resources.all.Resource;
-        // console.log(res.data);
-        console.log(resources);
-        //want Title and AccessibleVersion
-        displayResource(resources);
+        console.log(res.data.Result.Resources);
+        setResourceList(res.data.Result.Resources.all.Resource);
       })
       .catch((error) => console.log(error));
   };
@@ -45,6 +32,20 @@ export default function Resources() {
       <button onClick={() => generateResource(25, "female", "no", "yes", "no")}>
         Press Me for Resource
       </button>
+      <ul className="personalizedResources">
+        {Object.entries(resourceList).map((item) => {
+          // console.log(item);
+          let url = item.AccessibleVersion;
+          let title = item.Title;
+          console.log(item.Title);
+          return (
+            <li key={item.Id}>
+              <a href={url}>{JSON.stringify(item, null, 2)}</a>
+              {/* want item.title but that's undefined. */}
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 }
