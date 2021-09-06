f001e59da22a4cfbcbacd33faacd125aab3dd524
Giselle Rogers <giselle.b.rogers@gmail.com>
2021-09-06 17:07:51 -0600

    media queries added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,5 @@
 import styles from "./Footer.module.scss";
-import Triangle from "../../images/home-triangle.svg";
+
 export default function Footer() {
-  return (
-    <div className={styles.footer}>
-      Footer
-      <img src={Triangle}></img>
-    </div>
-  );
+  return <div className={styles.footer}>Footer</div>;
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,7 @@
 import styles from "./Home.module.scss";
 import MainSectionImg from "../../images/bc-types-home.jpg";
+import Triangle from "../../images/home-triangle.svg";
+import ThreeFaces from "../../images/threefaces.jpg";
 
 export default function Home() {
   return (
@@ -8,31 +10,45 @@ export default function Home() {
         <section className={styles.sectionOne}>
           <div>
             <div>
-              <h2>Take control of your health</h2>
+              <h1>My Body My Health</h1>
               <p>
-                Measure your cycle and get relevant information from trusted
-                sources.
+                Your health is so often put on the back burners. Put a stop to
+                that TODAY and grab hold to your health and your future. My Body
+                My Health allows you to take control of aspects of your health
+                such as menstrual tracking, contraceptive tracking, and provides
+                both personalized and generic health resources at the tips of
+                your fingers.
               </p>
               <button className={styles.getStartedBtn}>Get Started</button>
             </div>
             <img src={MainSectionImg} className={styles.mainSectionImg}></img>
           </div>
         </section>
-        <section className={styles.secondSection}>
-          {/* <h2>Section 2</h2> */}
-          <div className={styles.blueSection}></div>
-          <div className={styles.circleIcons}>
-            <div>Icon 1</div>
-            <div>Icon 2</div>
-            <div>Icon 3</div>
-            <div>Icon 4</div>
+        <section className={styles.sectionTwo}>
+          <div className={styles.blueSection}>
+            {/* <h2>Section 2</h2> */}
+            <div className={styles.circleIcons}>
+              <div>Icon 1</div>
+              <div>Icon 2</div>
+              <div>Icon 3</div>
+              <div>Icon 4</div>
+            </div>
           </div>
         </section>
-        <section>
+        <section className={styles.sectionThree}>
           <h2>Section 3</h2>
         </section>
+        <section className={styles.sectionFour}>
+          <div className={styles.mustardSqr}>
+            <h2>Explore Resources</h2>
+            <p>Get information by topic, or personalized just for you.</p>
+            <button className={styles.getStartedBtn}>Learn More</button>
+          </div>
+
+          <img src={ThreeFaces}></img>
+        </section>
         <section>
-          <h2>Section 4</h2>
+          <h2>Section 5</h2>
         </section>
       </div>
     </div>
