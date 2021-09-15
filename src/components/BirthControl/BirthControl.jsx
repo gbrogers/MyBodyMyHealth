d9b00f8c767bdba7bf5514b3bfea7bc3d1a8ce73
Giselle Rogers <giselle.b.rogers@gmail.com>
2021-09-14 20:42:05 -0600

    icons added

--- src/components/BirthControl/BirthControl.jsx
+++ src/components/BirthControl/BirthControl.jsx
@@ -3,6 +3,26 @@ import { useContext } from "react";
 import { UserContext } from "../../UserContext";
 import axios from "axios";
 
+import Abstinence from "../../images/abstinence.png";
+import BreastFeeding from "../../images/breastfeeding.png";
+import CervicalCap from "../../images/cervicalCap.png";
+import Condom from "../../images/condom.png";
+import Diaphragm from "../../images/diaphragm.png";
+import FAM from "../../images/FAM.png";
+import InternalCondom from "../../images/femaleCondom.png";
+import Implant from "../../images/implant.png";
+import IUD from "../../images/IUD.png";
+import NoMethod from "../../images/noMethod.png";
+import Patch from "../../images/patch.png";
+import Pills from "../../images/pills.png";
+import Scissors from "../../images/scissors.png";
+import Sponge from "../../images/sponge.png";
+import Sterilization from "../../images/sterilization.png";
+import Syringe from "../../images/syringe.png";
+import Spermicide from "../../images/spermicide.png";
+import VaginalRing from "../../images/vaginalRing.png";
+import Withdrawal from "../../images/withdrawal.png";
+
 export default function BirthControl(props) {
   const { user, setUser } = useContext(UserContext);
   const {
@@ -43,6 +63,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.mirena}
       >
+        <img src={IUD} alt="IUD"></img>
         <h3>IUD</h3>
         <h4>Mirena</h4>
         <p>Lasts Up To 7 years</p>
@@ -52,6 +73,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.kyleena}
       >
+        <img src={IUD} alt="IUD"></img>
         <h3>IUD</h3>
         <h4>Kyleena</h4>
         <p>Lasts Up To 5 years</p>
@@ -61,6 +83,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.liletta}
       >
+        <img src={IUD} alt="IUD"></img>
         <h3>IUD</h3>
         <h4>Liletta</h4>
         <p>Lasts Up To 7 years</p>
@@ -70,6 +93,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.skyla}
       >
+        <img src={IUD} alt="IUD"></img>
         <h3>IUD</h3>
         <h4>Skyla</h4>
         <p>Lasts Up To 3 years</p>
@@ -79,6 +103,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.paragard}
       >
+        <img src={IUD} alt="IUD"></img>
         <h3>IUD</h3>
         <h4>Paragard</h4>
         <p>Lasts Up To 12 years</p>
@@ -88,6 +113,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.implant}
       >
+        <img src={Implant} alt="implant"></img>
         <h3>Birth Control Implant</h3>
         <h4>Nexplanon</h4>
         <p>Lasts Up To 5 years</p>
@@ -97,6 +123,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.shot}
       >
+        <img src={Syringe} alt="syringe"></img>
         <h3>Birth Control Shot</h3>
         <h4>Depo-Provera</h4>
         <p>Recieve Every 3 Months</p>
@@ -106,6 +133,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.nuvaRing}
       >
+        <img src={VaginalRing} alt="vaginal ring"></img>
         <h3>Vaginal Ring</h3>
         <h4>NuvaRing</h4>
         <p>Replace Monthly</p>
@@ -115,6 +143,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.annovera}
       >
+        <img src={VaginalRing} alt="vaginal ring"></img>
         <h3>Vaginal Ring</h3>
         <h4>ANNOVERA</h4>
         <p>3 weeks in, 1 week out. Each ring used for up to 1 year.</p>
@@ -124,6 +153,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.xulane}
       >
+        <img src={Patch} alt="patch"></img>
         <h3>Birth Control Patch</h3>
         <h4>Xulane</h4>
         <p>Change Weekly. 3 weeks on, 1 off.</p>
@@ -133,6 +163,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.twirla}
       >
+        <img src={Patch} alt="patch"></img>
         <h3>Birth Control Patch</h3>
         <h4>Twirla</h4>
         <p>Change Weekly. 3 weeks on, 1 off.</p>
@@ -142,6 +173,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.combinationPill}
       >
+        <img src={Pills} alt="pills"></img>
         <h3>Birth Control Pill</h3>
         <h4>Combination Pills (Estrogen &amp; Progesterone)</h4>
         <p>Take Daily</p>
@@ -151,6 +183,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.progesteroneOnly}
       >
+        <img src={Pills} alt="pills"></img>
         <h3>Birth Control Pill</h3>
         <h4>Progesterone-Only Pills (mini pills)</h4>
         <p>Take Daily</p>
@@ -160,6 +193,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.condom}
       >
+        <img src={Condom} alt="condom"></img>
         <h3>Condom</h3>
         <p>Use Every Time</p>
       </button>
@@ -168,6 +202,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.internalCondom}
       >
+        <img src={InternalCondom} alt="internal condom"></img>
         <h3>Internal Condom</h3>
         <p>Use Every Time</p>
       </button>
@@ -176,6 +211,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.diaphragm}
       >
+        <img src={Diaphragm} alt="diaphragm"></img>
         <h3>Diaphragm</h3>
         <p>Use Every Time</p>
       </button>
@@ -184,6 +220,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.sponge}
       >
+        <img src={Sponge} alt="sponge"></img>
         <h3>Birth Control Sponge</h3>
         <p>Use Every Time</p>
       </button>
@@ -192,6 +229,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.spermicide}
       >
+        <img src={Spermicide} alt="spermicide"></img>
         <h3>Spermicide &amp; Gel</h3>
         <p>Use Every Time</p>
       </button>
@@ -200,6 +238,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.cervicalCap}
       >
+        <img src={CervicalCap} alt="cervical cap"></img>
         <h3>Cervical Cap</h3>
         <p>Use Every Time</p>
       </button>
@@ -208,6 +247,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.fertilityAwareness}
       >
+        <img src={FAM} alt="fertility awareness"></img>
         <h3>Fertility Awareness</h3>
         <p>Use Constantely</p>
       </button>
@@ -216,6 +256,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.withdrawal}
       >
+        <img src={Withdrawal} alt="withdrawal"></img>
         <h3>Withdrawal (Pull Out Method)</h3>
         <p>Use Every Time</p>
       </button>
@@ -224,6 +265,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.breastfeeding}
       >
+        <img src={BreastFeeding} alt="breastfeeding"></img>
         <h3>Breastfeeding as Birth Control</h3>
         <p>Every 4-5 hours</p>
       </button>
@@ -232,6 +274,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.abstinence}
       >
+        <img src={Abstinence} alt="abstinence"></img>
         <h3>Outercourse &amp; Abstinence</h3>
         <p>Use Every Time</p>
       </button>
@@ -240,6 +283,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.sterilization}
       >
+        <img src={Sterilization} alt="sterilization"></img>
         <h3>Sterilization</h3>
         <p>Lasts for Life</p>
       </button>
@@ -248,6 +292,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.vasectomy}
       >
+        <img src={Scissors} alt="scissors"></img>
         <h3>Vasectomy</h3>
         <p>Lasts for Life</p>
       </button>
@@ -256,6 +301,7 @@ export default function BirthControl(props) {
         onClick={(e) => handleSelection(e, "value")}
         className={styles.nothing}
       >
+        <img src={NoMethod} alt="no method"></img>
         <h3>No Method</h3>
         <p>Not very effective...</p>
       </button>
