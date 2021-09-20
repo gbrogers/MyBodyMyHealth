3438ac496f8ede0ff00b2af4c054bbd7a081798f
Giselle Rogers <giselle.b.rogers@gmail.com>
2021-09-20 16:41:51 -0600

    fix build pointing

--- server/server.js
+++ server/server.js
@@ -31,10 +31,10 @@ app.get("/api/getLastBCUse/:user_id", ctrl.getLastBCUse);
 connect.connect();
 
 // if (process.env.NODE_ENV == "production") {
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "../build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "../public/index.html"));
+  res.sendFile(path.join(__dirname, "../build", "../public/index.html"));
 });
 // }
 
