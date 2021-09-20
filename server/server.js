1dbac4de4e03061434782cf1783000a669b5376f
Giselle Rogers <giselle.b.rogers@gmail.com>
2021-09-20 16:31:37 -0600

    update to server.js

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.get("/api/getLastBCUse/:user_id", ctrl.getLastBCUse);
 connect.connect();
 
 // if (process.env.NODE_ENV == "production") {
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "../public/index.html"));
