40e9e78c79484259481d242a0cf0136b915dee77
Giselle Rogers <giselle.b.rogers@gmail.com>
2021-09-20 19:17:44 -0600

    update to production conditional

--- server/server.js
+++ server/server.js
@@ -4,17 +4,19 @@ const ctrl = require("./controller/controller.js");
 const app = express();
 
 const path = require("path");
-app.use(express.static(path.join(__dirname, "build")));
 
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "../public"));
+  });
+}
 const connect = require("./database/db.js");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "../public"));
-});
 
 // endpoints
 app.post("/api/authenticate/login", ctrl.loginUser);
