bb36ddf104e462d488d84c30b5041c14dde44b22
Giselle Rogers <giselle.b.rogers@gmail.com>
2021-09-21 16:18:44 -0600

    deployment test

--- server/server.js
+++ server/server.js
@@ -6,10 +6,10 @@ const app = express();
 const path = require("path");
 
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "build")));
-
+  //set static folder
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "../public"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 const connect = require("./database/db.js");
