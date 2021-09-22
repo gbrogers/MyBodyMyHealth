e7c7917b54e9fd3aa4b4d9dfccb6005ee3001f89
Giselle Rogers <giselle.b.rogers@gmail.com>
2021-09-22 13:17:06 -0600

    remove sequelize call

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const ctrl = require("./controller/controller.js");
+// const ctrl = require("./controller/controller.js");
 const app = express();
 const path = require("path");
 
